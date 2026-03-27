a4bb79bae8e41c4f88d65644d50d735e559c1e1c
maju0029 <maju0029@gmail.com>
2026-03-27 10:06:26 +0100

    start of random word api

--- react-word-game/server/index.js
+++ react-word-game/server/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-
-const app = express();
-const PORT = 5080;
+import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const data = fs.readFileSync(
+  path.join (__dirname, "./words_dictionary.json"),
+);
+const wordsData = JSON.parse(data);
+const words = Object.keys(wordsData);
+
+const app = express();
+const PORT = 5080;
+
 app.use(express.json());
 
-app.get("/api/test", (req, res) => {
-  res.json({ message: "API funkar!" });
+app.get("/api/word", (req, res) => {
+  const randomWord = words [Math.floor(Math.random() * words.length)];
+  res.json({ word: randomWord });
 });
 
 app.use(express.static(path.join(__dirname, "../dist")));
--- react-word-game/src/App.jsx
+++ react-word-game/src/App.jsx
@@ -7,11 +7,18 @@ function App() {
   return (
     <main class="app">
     <h1>React Word Game</h1>
+    <ul>
+      <li>Highscore</li>
+      <li>Information about the projekt</li>
+    </ul>
     <p>Välkommen till React Word Game! Här kan du spela ett roligt ordspel och utmana dina vänner. Klicka på knappen nedan för att starta spelet och se hur många ord du kan hitta!</p>
     <button class="start-button" onClick={() => setCount(count + 1)}>
       Starta Spelet
     </button>
     <p>Du har startat spelet {count} gånger.</p>
+    <p></p>
+    <input type="text" placeholder="hur många bokstäver?" class="word-input"
+    />
     </main>
   )
 }
