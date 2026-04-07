2088f4da1b04bca44335ac16494eaa852dffc168
maju0029 <maju0029@gmail.com>
2026-04-07 11:54:26 +0200

    install mongoose, make a connection to db

--- react-word-game/server/index.js
+++ react-word-game/server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
+import { connectDB } from "./database/db.js";
 
 const PORT = 5080;
 const __filename = fileURLToPath(import.meta.url);
@@ -19,10 +20,16 @@ setWords(words);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("/"*"}", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
